690db8da8583c61387c9f2c16a985ec8ab7a853c
Maria Hagelin <mthagelin@gmail.com>
2023-05-29 10:21:32 +0200

    removed from address NotBlank lol

--- src/main/java/com/example/customerservice/Models/Customer.java
+++ src/main/java/com/example/customerservice/Models/Customer.java
@@ -3,6 +3,7 @@ package com.example.customerservice.Models;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -27,7 +28,6 @@ public class Customer {
     @Size(min = 9, max = 13, message = "Ssn must be unique and between 9 and 13 characters")
     private String ssn;
     @Embedded
-    @NotBlank(message = "Must not be blank")
     private Address address;
     @Email(message = "E-mail must be set properly")
     private String email;
