c15c68db1fe0d53244c9f3e580300998079fa195
AnnaSjoberg <anna.sjoberg@yh.nackademin.se>
2023-05-30 14:57:59 +0200

    documentation w springdoc openapi

--- src/main/java/com/example/customerservice/CustomerServiceApplication.java
+++ src/main/java/com/example/customerservice/CustomerServiceApplication.java
@@ -1,22 +1,37 @@
 package com.example.customerservice;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Customer Service API",
+                version = "2.0.0",
+                description = "API for managing customers in the web shop",
+                contact = @Contact(
+                        name = "Customer department",
+                        email = "customer.department@ourWebshop.com"
+                )
+        )
+)
 @SpringBootApplication
-//@OpenAPIDefinition
+
 public class CustomerServiceApplication {
 
-	@Bean
-	RestTemplate restTemplate() {
-		RestTemplate restTemplate = new RestTemplate();
-		return restTemplate;
-	}
-	public static void main(String[] args) {
-		SpringApplication.run(CustomerServiceApplication.class, args);
-	}
+    @Bean
+    RestTemplate restTemplate() {
+        RestTemplate restTemplate = new RestTemplate();
+        return restTemplate;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerServiceApplication.class, args);
+    }
 
 }
